6a19f61587e7f55b3736a0d1b7ef676cb0c79191
Jinsoyeonn <wlsdmsquf@nate.com>
2024-05-17 14:40:39 +0900

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,20 @@
-var express = require('express');
-var router = express.Router();
+var express = require("express");
+var app = express();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index.html');
-});
+app.get("/", (req, res) => {
+    res.send(
+        "<p>안녕하세요</p>"
+    )
+})
+app.listen(3000, (err)=> {
+    console.log(3000);
+})
+// var express = require('express');
+// var router = express.Router();
 
-module.exports = router;
+// /* GET home page. */
+// router.get('/', function(req, res, next) {
+//   res.render('index.html');
+// });
+
+// module.exports = router;
