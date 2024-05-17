c61ee1a70e105474779952917462b900e8f8818d
Jinsoyeonn <wlsdmsquf@nate.com>
2024-05-17 14:01:27 +0900

    Update test.js

--- bin/test.js
+++ bin/test.js
@@ -1,11 +1,11 @@
-// var express = require("express");
-// var app = express();
+var express = require("express");
+var app = express();
 
-// app.get("/", (req, res) => {
-//     res.send(
-//         "<p>안녕하세요</p>"
-//     )
-// })
-// app.listen(3000, (err)=> {
-//     console.log(3000);
-// })
+app.get("/", (req, res) => {
+    res.send(
+        "<p>안녕하세요</p>"
+    )
+})
+app.listen(3000, (err)=> {
+    console.log(3000);
+})
